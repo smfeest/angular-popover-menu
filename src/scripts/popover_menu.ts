623b3eb9bd9854fab9d5504845fa25821abf0d63
Stephen Feest <stephen.feest@futurelearn.com>
2016-08-17 19:13:35 +0100

    Open and close popover on button click

--- src/scripts/popover_menu.ts
+++ src/scripts/popover_menu.ts
@@ -1,13 +1,17 @@
 angular.module('sfPopoverMenu', []).directive('sfPopoverMenu', popoverMenuDirective);
 
 function popoverMenuDirective(): ng.IDirective {
+    let counter = 0;
+
     return {
         link: (scope, element) => {
             const id = element[0].id = (element[0].id || ('popover-menu-' + counter++));
 
             element.addClass('popover-menu');
 
-            const button = element.children('button').attr('aria-haspopup', 'true');
+            const button = element.children('button')
+                .attr('aria-haspopup', 'true')
+                .on('click', toggle);
             const buttonElement = <HTMLButtonElement>button[0];
             const buttonId = buttonElement.id = id + '__button';
             buttonElement.type = 'button';
@@ -15,8 +19,29 @@ function popoverMenuDirective(): ng.IDirective {
             element.children('ul')
                 .addClass('popover-menu__list')
                 .attr('aria-labelledby', buttonId);
+
+            close();
+
+            const OPEN_MODIFIER = 'popover-menu--open';
+            const EXPANDED_ATTRIBUTE = 'aria-expanded';
+
+            function toggle(): void {
+                if (element.hasClass(OPEN_MODIFIER)) {
+                    close();
+                } else {
+                    open();
+                }
+            }
+
+            function open(): void {
+                element.addClass(OPEN_MODIFIER);
+                button.attr(EXPANDED_ATTRIBUTE, 'true');
+            }
+
+            function close(): void {
+                element.removeClass(OPEN_MODIFIER);
+                button.attr(EXPANDED_ATTRIBUTE, 'false');
+            }
         },
     };
 }
-
-let counter = 0;
