38b93da1b95cd4e29d5c0699cd5d2e80cc84082a
Stephen Feest <stephen.feest@futurelearn.com>
2016-08-17 19:03:42 +0100

    Initialize popover menu parts
    Adds a link function to the `sf-popover-menu` directive to:
    
    + Assign an ID to the drop down container (if it doesn't already have one)
    + Generate and assign an ID to the button element
    + Add a `labelledby` attribute to indicate that the list is labelled by the
      button
    + Add initial classes and ARIA attributes to the container, button and list

--- src/scripts/popover_menu.ts
+++ src/scripts/popover_menu.ts
@@ -1,5 +1,22 @@
 angular.module('sfPopoverMenu', []).directive('sfPopoverMenu', popoverMenuDirective);
 
 function popoverMenuDirective(): ng.IDirective {
-    return {};
+    return {
+        link: (scope, element) => {
+            const id = element[0].id = (element[0].id || ('popover-menu-' + counter++));
+
+            element.addClass('popover-menu');
+
+            const button = element.children('button').attr('aria-haspopup', 'true');
+            const buttonElement = <HTMLButtonElement>button[0];
+            const buttonId = buttonElement.id = id + '__button';
+            buttonElement.type = 'button';
+
+            element.children('ul')
+                .addClass('popover-menu__list')
+                .attr('aria-labelledby', buttonId);
+        },
+    };
 }
+
+let counter = 0;
